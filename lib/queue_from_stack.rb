1a4c6fd377500164de07bbbdf31847985e3dcea7
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-04-21 15:47:38 -0400

    commit tree 0920c8dade68976a5a17c5fd53e005be38e10611

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,25 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(value)
+       if @s1.empty? == true
+        @s1.push(value)
+       end
+       pop = @s1.pop
+       puts pop
+    end
+
+    def remove
+        @s1.pop
+    end
+
+    def peek
+        # puts @s2.peek
+    end
+end
